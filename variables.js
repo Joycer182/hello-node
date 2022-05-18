a1e7db451aa0226ca10056b0883927831bd6604a
Jose hernandez <hernandez.jose85@gmail.com>
2022-05-18 16:20:05 -0400

    json is acuaman

---
+++ variables.js
@@ -0,0 +1,22 @@
+let edad = 38; // number
+
+let nombre = "Victor"; // string
+
+const isActive = true; // boolean
+
+let myArray = [ [1, 2, 3], edad, nombre, isActive];
+
+const victor = require("./momoa.json");
+
+console.log(victor);
+
+const imprimirArray = (_array) => {
+    _array.forEach((item, idx) => {
+        console.log(idx, item, typeof item);
+    })
+}
+
+victor.imprimir = imprimirArray;
+console.log(victor);
+
+console.log(victor.imprimir(victor.balance));
