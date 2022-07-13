9f856b5cf3a854899edd7b8cf237759f732ec8e6
Jose hernandez <hernandez.jose85@gmail.com>
2022-07-13 19:56:15 -0400

    Acepte los cambios y el codigo de fortmateo de otra manera

--- gestionbd.js
+++ gestionbd.js
@@ -5,7 +5,6 @@ import mysql from "mysql2/promise.js";
 // Fuente de ayuda: https://evertpot.com/executing-a-mysql-query-in-nodejs/
 
 // Credenciales para acceder a la Base de Datos.
-// const connection = await mysql.createConnection({
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
@@ -47,7 +46,6 @@ export const VerificaRegistroBD = async (Registro) => {
 };
 
 // Inserta valores de interés a la Base de Datos.
-<<<<<<< Updated upstream
 export const InsertarInformacion = async (
   symbol,
   price,
@@ -79,21 +77,6 @@ export const InsertarInformacion = async (
     console.error(e);
     return [];
   }
-=======
-export const InsertarInformacion = async ( symbol , price , priceChangePercent30min , priceChangePercent24hr, volumeChange24hr , name , ath , ath_change_percentage , market_cap_rank , SentidoMovimiento ) => {
-    try {
-        //await connection.execute(connection , `INSERT INTO CryptoVariacion (symbol , price , priceChangePercent30min , priceChangePercent24hr, volumeChange24hr, name, ath, ath_change_percentage, market_cap_rank, SentidoMovimiento) VALUES (${mysql.escape(symbol)} , ${mysql.escape(price)} , ${mysql.escape(priceChangePercent30min)} , ${mysql.escape(priceChangePercent24hr)} , ${mysql.escape(volumeChange24hr)} , ${mysql.escape(name)} , ${mysql.escape(ath)} , ${mysql.escape(ath_change_percentage)} , ${mysql.escape(market_cap_rank)} , ${mysql.escape(SentidoMovimiento)})`);
-        // await connection.query(`INSERT INTO CryptoMapa.CryptoVariacion (symbol, price, priceChangePercent30min, priceChangePercent24hr, volumeChange24hr, name, ath, ath_change_percentage, market_cap_rank, SentidoMovimiento) VALUES (${mysql.escape(symbol)}, ${mysql.escape(price)}, ${mysql.escape(priceChangePercent30min)}, ${mysql.escape(priceChangePercent24hr)}, ${mysql.escape(volumeChange24hr)}, ${mysql.escape(name)}, ${mysql.escape(ath)}, ${mysql.escape(ath_change_percentage)}, ${mysql.escape(market_cap_rank)}, ${mysql.escape(SentidoMovimiento)}) ON DUPLICATE KEY UPDATE symbol = ${mysql.escape(symbol)}, price = ${mysql.escape(price)}, priceChangePercent30min = ${mysql.escape(priceChangePercent30min)}, priceChangePercent24hr = ${mysql.escape(priceChangePercent24hr)}, volumeChange24hr = ${mysql.escape(volumeChange24hr)}, name = ${mysql.escape(name)}, ath = ${mysql.escape(ath)}, ath_change_percentage = ${mysql.escape(ath_change_percentage)}, market_cap_rank = ${mysql.escape(market_cap_rank)}, SentidoMovimiento = ${mysql.escape(SentidoMovimiento)};`);
-        console.log('Si entre a la funcion insertar');
-        await connection.execute(`INSERT INTO CryptoMapa.CryptoVariacion (symbol, price, priceChangePercent30min, priceChangePercent24hr, volumeChange24hr, name, ath, ath_change_percentage, market_cap_rank, SentidoMovimiento) VALUES (${mysql.escape(symbol)}, ${mysql.escape(price)}, ${mysql.escape(priceChangePercent30min)}, ${mysql.escape(priceChangePercent24hr)}, ${mysql.escape(volumeChange24hr)}, ${mysql.escape(name)}, ${mysql.escape(ath)}, ${mysql.escape(ath_change_percentage)}, ${mysql.escape(market_cap_rank)}, ${mysql.escape(SentidoMovimiento)}) AS valores ON DUPLICATE KEY UPDATE price = valores.price, priceChangePercent30min = valores.priceChangePercent30min, priceChangePercent24hr = valores.priceChangePercent24hr, volumeChange24hr = valores.volumeChange24hr, name = valores.name, ath = valores.ath, ath_change_percentage = valores.ath_change_percentage, market_cap_rank = valores.market_cap_rank, SentidoMovimiento = valores.SentidoMovimiento`);
-        console.log("Información agregada correctamente a la Base de Datos.");
-        return [];
-    } catch (e) {
-        console.log("Error en la inserción de información a la Base de Datos.");
-        console.error(e);
-        return [];
-    };
->>>>>>> Stashed changes
 };
 
 // Actualiza un registro especifo de la Base de Datos.
@@ -141,7 +124,6 @@ export const ActualizarRegistro = async (
 
 const TEST = await LeerTodoBD();
 console.log(TEST[0].length);
-<<<<<<< Updated upstream
 await InsertarInformacion(
   "ANCBUSD",
   4000.45,
@@ -170,17 +152,6 @@ const datos = Monedas.map((MonedaEstudio) => {
     SentidoMovimiento: 1,
   };
 });
-=======
-//await InsertarInformacion('ANCBUSD',4000.45,41.25,78.56,55599.25,'MonedaEstudio',3.09,-84.57006,1,1);
-
-    const Monedas = ['BTCUSDT','ETHUSDT','BTCBUSD','ANCBUSD','XMRUSDT'];
-    for ( let m = 0; m<Monedas.length; m++ ) {
-        let MonedaEstudio = Monedas[m];
-        await InsertarInformacion(MonedaEstudio,6.45,41.25,78.56,55599.25,MonedaEstudio,3.09,-84.57006,1,1);
-    };
-
-    process.exit(0);
->>>>>>> Stashed changes
 
 process.exit(0);
 
